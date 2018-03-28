8889568365ee3956bb8fc8757b71e3b2383e800b
Mohammad Shawahneh <mohammad.s.shawahneh@gmail.com>
2018-03-28 04:14:52 +0300

    fix getTransportation dateTime Error based on previous commit by maysara

--- api/methods.php
+++ api/methods.php
@@ -670,7 +670,7 @@ AND
 
             array_push($catch,array("id"=>$r["id"],
                 "type"=>$r["type"],
-                "time"=>$r["timegoind"]));
+                "time"=>date("g:i a", strtotime($r["timegoing"]))));
         }
         return json_encode(array("fromRawabi"=>$fromRawabi,"fromRamallah"=>$fromRamallah));
     }
